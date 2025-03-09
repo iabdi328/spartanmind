b7c087ae13ea11650fd3cd78256c243bb5e4647a
ismail <abdiisma@msu.edu>
2025-03-09 04:24:45 -0400

    created ids.h file

---
+++ SpartanmindLib/ids.h
@@ -0,0 +1,24 @@
+/**
+ * @file ids.h
+ * @author ismail
+ *
+ *
+ */
+ 
+#ifndef IDS_H
+#define IDS_H
+
+// Enum to define custom IDs for levels and other menu items
+enum IDs {
+ // Level IDs - used for loading different levels
+ IDM_LOAD_LEVEL0 = wxID_HIGHEST + 1,  // Start custom IDs from the highest wxWidgets ID
+ IDM_LOAD_LEVEL1,
+ IDM_LOAD_LEVEL2,
+ IDM_LOAD_LEVEL3,
+
+ // Standard Menu Item IDs
+ IDM_EXIT,          // ID for the "Exit" menu item
+ IDM_ABOUT          // ID for the "About" menu item
+};
+
+#endif //IDS_H
