31ccae3bde821ffafe615810b457d4435e915916
ismail <abdiisma@msu.edu>
2025-03-11 03:08:11 -0400

    timer/scoreboard added.

---
+++ SpartanmindLib/Scoreboard.cpp
@@ -0,0 +1,34 @@
+/**
+* @file Scoreboard.cpp
+ * @author ismail
+ */
+#include "Scoreboard.h"
+#include <wx/font.h>
+#include <wx/colour.h>
+
+Scoreboard::Scoreboard()
+    : mElapsedTime(0.0)
+{
+}
+
+void Scoreboard::Update(double deltaSeconds) {
+ mElapsedTime += deltaSeconds;
+}
+
+void Scoreboard::Draw(std::shared_ptr<wxGraphicsContext> gc) const {
+ // Set up a bold font for the scoreboard.
+ wxFont font(ScoreboardTextSize, wxFONTFAMILY_DEFAULT, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+ gc->SetFont(font, *wxWHITE);
+
+ // Convert elapsed time into minutes and seconds.
+ int minutes = static_cast<int>(mElapsedTime) / 60;
+ int seconds = static_cast<int>(mElapsedTime) % 60;
+ wxString timeStr = wxString::Format("%02d:%02d", minutes, seconds);
+
+ // Draw the time at the fixed virtual position (ScoreboardTopLeft).
+ gc->DrawText(timeStr, ScoreboardTopLeft.x, ScoreboardTopLeft.y);
+}
+
+double Scoreboard::GetElapsedTime() const {
+ return mElapsedTime;
+}
---
+++ SpartanmindLib/Scoreboard.h
@@ -0,0 +1,35 @@
+/**
+ * @file Scoreboard.h
+ * @author ismail
+ *
+ *
+ */
+
+#ifndef SCOREBOARD_H
+#define SCOREBOARD_H
+
+#include <wx/graphics.h>
+#include "memory"
+
+// Define the scoreboard constants.
+const int ScoreboardTextSize = 50;
+const wxPoint ScoreboardTopLeft(10, 10);
+
+class Scoreboard {
+public:
+ Scoreboard();
+
+ // Update the elapsed time (deltaSeconds is the time elapsed since the last update).
+ void Update(double deltaSeconds);
+
+ // Draw the scoreboard (elapsed time in MM:SS format) using the provided graphics context.
+ void Draw(std::shared_ptr<wxGraphicsContext> gc) const;
+
+ // Optionally, get the elapsed time (in seconds).
+ double GetElapsedTime() const;
+
+private:
+ double mElapsedTime; // Total elapsed time in seconds.
+};
+
+#endif //SCOREBOARD_H
