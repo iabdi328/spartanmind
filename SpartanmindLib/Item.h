88c3b07faf8f94349321a2e8130848c683b5a541
ismail <abdiisma@msu.edu>
2025-03-16 08:27:11 -0400

    added #include <memory>

--- SpartanmindLib/Item.h
+++ SpartanmindLib/Item.h
@@ -1,5 +1,5 @@
 /**
- * @file Item.h
+* @file Item.h
  * @author Daniel Conti
  * @author Raj Ambekar
  *
@@ -9,6 +9,7 @@
 #define ITEM_H
 #include <string>
 #include <wx/image.h>
+#include <memory>
 
 class wxDC;
 class Spartanmind;
@@ -16,63 +17,63 @@ class Spartanmind;
 class Item {
 
 private:
-    /// The underlying fish image
-    std::unique_ptr<wxImage> mItemImage;
+ /// The underlying fish image
+ std::unique_ptr<wxImage> mItemImage;
 
-    /// The bitmap we can display for this fish
-    std::unique_ptr<wxBitmap> mItemBitmap;
+ /// The bitmap we can display for this fish
+ std::unique_ptr<wxBitmap> mItemBitmap;
 
-    /// The spartanmind this item is contained in
-    Spartanmind   *mSpartanmind;
+ /// The spartanmind this item is contained in
+ Spartanmind   *mSpartanmind;
 
-    /// Item location in the spartanmind
-    double  mX = 0;     ///< X location for the center of the item
-    double  mY = 0;     ///< Y location for the center of the item
+ /// Item location in the spartanmind
+ double  mX = 0;     ///< X location for the center of the item
+ double  mY = 0;     ///< Y location for the center of the item
 protected:
-    Item(Spartanmind* spartanmind, const std::wstring &filename);
+ Item(Spartanmind* spartanmind, const std::wstring &filename);
 
 public:
 
-    /// Default constructor (disabled)
-    Item() = delete;
-    /// Copy constructor (disabled)
-    Item(const Item &) = delete;
-    /// Assignment operator
-    void operator=(const Item &) = delete;
-    virtual ~Item();
+ /// Default constructor (disabled)
+ Item() = delete;
+ /// Copy constructor (disabled)
+ Item(const Item &) = delete;
+ /// Assignment operator
+ void operator=(const Item &) = delete;
+ virtual ~Item();
 
-    /**
-      * The X location of the item
-      * @return X location in pixels
-      */
-    double GetX() const { return mX; }
+ /**
+   * The X location of the item
+   * @return X location in pixels
+   */
+ double GetX() const { return mX; }
 
-    /**
-     * The Y location of the item
-     * @return Y location in pixels
-     */
-    double GetY() const { return mY; }
+ /**
+  * The Y location of the item
+  * @return Y location in pixels
+  */
+ double GetY() const { return mY; }
 
-    /**
-     * Set the item location
-     * @param x X location in pixels
-     * @param y Y location in pixels
-     */
-    void SetLocation(double x, double y) { mX = x; mY = y; }
+ /**
+  * Set the item location
+  * @param x X location in pixels
+  * @param y Y location in pixels
+  */
+ void SetLocation(double x, double y) { mX = x; mY = y; }
 
-    /**
-     * Test this item to see if it has been clicked on
-     * @param x X location on the spartanmind to test in pixels
-     * @param y Y location on the spartanmind to test in pixels
-     * @return true if clicked on
-     */
-    bool HitTest(int x, int y);
+ /**
+  * Test this item to see if it has been clicked on
+  * @param x X location on the spartanmind to test in pixels
+  * @param y Y location on the spartanmind to test in pixels
+  * @return true if clicked on
+  */
+ bool HitTest(int x, int y);
 
-    /**
-     * Draw this item
-     * @param dc Device context to draw on
-     */
-    void Draw(wxDC *dc);
+ /**
+  * Draw this item
+  * @param dc Device context to draw on
+  */
+ void Draw(wxDC *dc);
 };
 
 #endif //ITEM_H
